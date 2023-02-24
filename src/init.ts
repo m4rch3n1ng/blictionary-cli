8b5dc2c9d41cbcefff816e8d8037a20cd53389fb
m4rch <m4rch3n1ng@gmail.com>
2023-02-24 02:23:33 +0100

    make messages.word-rank.txt

--- src/init.ts
+++ src/init.ts
@@ -3,6 +3,7 @@ import { existsSync } from "node:fs"
 import { mkdir, writeFile } from "node:fs/promises"
 import { join as joinPath } from "node:path"
 import { extractDiscord, extractIrc, extractTwitch } from "./utils/collect.js"
+import { rankWords } from "./utils/rank.js"
 
 export default async function analyze ( discordZip: string, twitchDir: string, ircTxt: string, outDir: string = ".", { zip }: { zip: boolean }) {
 	if (!existsSync(discordZip) || !/\.7z$/.test(discordZip)) throw `cannot find file ${discordZip}`
@@ -13,6 +14,7 @@ export default async function analyze ( discordZip: string, twitchDir: string, i
 	const sortedMessages = [ discord, twitch, irc ].flat().sort(( msg1, msg2 ) => +new Date(msg1.date) - +new Date(msg2.date))
 
 	const messageTxt = sortedMessages.map(( message ) => ` ${message.text.replace(/\r?\n/g, " ")} `.replace(/  +/g, " ")).join("\n")
+	const wordsRank = rankWords(messageTxt)
 
 	const outName = `init.${new Date().toISOString().replace(/:/g, "-")}`
 	const outPath = joinPath(outDir, outName)
@@ -21,6 +23,7 @@ export default async function analyze ( discordZip: string, twitchDir: string, i
 
 	await writeFile(joinPath(outPath, "messages.json"), JSON.stringify(sortedMessages))
 	await writeFile(joinPath(outPath, "messages.txt"), messageTxt)
+	await writeFile(joinPath(outPath, "messages.word-rank.txt"), wordsRank)
 
 	if (zip) {
 		await new Promise<void>(( resolve, reject ) => (
