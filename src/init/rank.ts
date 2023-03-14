82e86723ef3e98071e3e7ae0fe1f90da7bf52ff8
m4rch <m4rch3n1ng@gmail.com>
2023-03-14 20:28:20 +0100

    refine word split for word rank

--- src/init/rank.ts
+++ src/init/rank.ts
@@ -1,13 +1,17 @@
 export function rankWords ( messagesTxt: string ) {
-	const sanitized = messagesTxt.replace(/\r?\n/g, " ").replace(/["“”´`#€£$%;,*+=!?^_~|{}()\[\]\/\\]/g, " ")
-		.replace(/([^<]):(\d*[^\d>])/g, "$1 $2")
-		.replace(/([^<])@/g, "$1 ")
-		.replace(/([^@])&/g, "$1 ")
-		.replace(/([^a-z])[']([^a-z])/g, "$1 $2").replace(/([^a-z])[']([a-z])/g, "$1 $2").replace(/([a-z])[']([^a-z])/g, "$1 $2")
-		.replace(/(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g, " $1 ")
-		.replace(/(\D)\.+(\D)/g, "$1 $2").replace(/(\D)\.+(\d)/g, "$1 $2").replace(/(\d)\.+(\D)/g, "$1 $2")
-	
-	const words = sanitized.split(/ +/g).map(( str ) => str.toLowerCase()).filter(( w ) => w)
+	const sanitized = messagesTxt
+		.replace(/\r?\n/g, " ") // rm newline
+		.replace(/["“”´`#€£$%;,*+=!?^_~|{}()\[\]\/\\]/g, " ") // rm punctuation
+		.replace(/’/g, "'") // convert ’ to '
+		.replace(/>(.)/g, "> $1") // seperate >.
+		.replace(/([^<a]):(\d*[^\d>])/g, "$1 $2") // keep custom discord emotes
+		.replace(/([^<])@/g, "$1 ") // keep user pings
+		.replace(/([^@])&/g, "$1 ") // keep role pings
+		.replace(/([^a-z])[']([^a-z])/gi, "$1 $2").replace(/([^a-z])[']([a-z])/gi, "$1 $2").replace(/([a-z])[']([^a-z])/gi, "$1 $2") // only keep [a-z]'[a-z]
+		.replace(/(\D)\.+(\D)/g, "$1 $2").replace(/(\D)\.+(\d)/g, "$1 $2").replace(/(\d)\.+(\D)/g, "$1 $2") // only keep \d\.\d
+		.replace(/(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g, " $1 ") // seperate emoji
+
+	const words = sanitized.split(/\s+/g).map(( str ) => str.toLowerCase()).filter(( w ) => w)
 	const map = new Map<string, number>
 
 	words.forEach(( word ) => {
